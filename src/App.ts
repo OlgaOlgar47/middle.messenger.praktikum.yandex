6732b8c9ec013d8521c77e47d94d39bb6dc00cb9
Olga Averianova <OlgaOlgar47@yandex.ru>
2025-07-31 00:45:25 -0300

    feat: Prettierrc

--- src/App.ts
+++ src/App.ts
@@ -1,17 +1,18 @@
-import "./templates/partials";
-import { renderRoute } from "./router";
-import { renderLayout } from "./components/Layout";
+import './templates/partials';
+import { renderRoute } from './router';
+import { renderLayout } from './components/Layout';
 
 export function App() {
   function updateView() {
-    const root = document.querySelector("#app");
+    const root = document.querySelector('#app');
     if (!root) return;
     const page = renderRoute();
     root.innerHTML = renderLayout(page);
   }
 
-  document.addEventListener("DOMContentLoaded", () => {
-    window.addEventListener("hashchange", updateView);
+  document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('hashchange', updateView);
     updateView();
   });
 }
+

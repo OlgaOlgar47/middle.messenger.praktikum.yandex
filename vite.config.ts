df20fa057dea2aa165ed8f69b651229f2d27e439
Olga Averianova <OlgaOlgar47@yandex.ru>
2025-07-31 22:10:49 -0300

    fix: remove context from config

--- vite.config.ts
+++ vite.config.ts
@@ -7,25 +7,6 @@ export default defineConfig({
   plugins: [
     handlebars({
       partialDirectory: "./src/templates",
-      defaultLayout: false,
-      context: page => {
-        switch (page) {
-          case "login":
-            return { title: "Авторизация" };
-          case "register":
-            return { title: "Регистрация" };
-          case "chats":
-            return { title: "Чаты" };
-          case "profile":
-            return { title: "Профиль" };
-          case "404":
-            return { title: "404 - Не найдено" };
-          case "500":
-            return { title: "5** - Ошибка сервера" };
-          default:
-            return { title: "Мессенджер" };
-        }
-      },
     }),
   ],
   server: {

30aea2f8afdf358a116d9e8e1175d70841904f85
Olga Averianova <OlgaOlgar47@yandex.ru>
2025-08-01 19:48:26 -0300

    fix: global styles + authForm styles

--- src/pages/Login/index.ts
+++ src/pages/Login/index.ts
@@ -1,6 +1,6 @@
 import Handlebars from "handlebars";
 import rawTemplate from "./Login.hbs?raw";
-import styles from "./Login.module.sass";
+import styles from "../styles/authForm.module.sass";
 
 export function renderLoginPage() {
   const template = Handlebars.compile(rawTemplate);
--- src/pages/Register/index.ts
+++ src/pages/Register/index.ts
@@ -1,6 +1,6 @@
 import Handlebars from "handlebars";
 import rawTemplate from "./Register.hbs?raw";
-import styles from "./Register.module.sass";
+import styles from "../styles/authForm.module.sass";
 
 export function renderRegisterPage() {
   const template = Handlebars.compile(rawTemplate);
